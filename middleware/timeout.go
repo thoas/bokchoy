da6cd3739b943291fe6ed4a2545ac3d6369f0c92
Florent Messa <florent.messa@gmail.com>
2019-07-11 19:00:15 +0200

    feat: add Timeout middleware

---
+++ middleware/timeout.go
@@ -0,0 +1,54 @@
+// inspired from https://github.com/go-chi/chi/blob/master/middleware/timeout.go
+
+package middleware
+
+import (
+	"context"
+	"time"
+
+	"github.com/thoas/bokchoy"
+)
+
+// Timeout is a middleware that cancels ctx after a given timeout and return
+//
+// It's required that you select the ctx.Done() channel to check for the signal
+// if the context has reached its deadline and return, otherwise the timeout
+// signal will be just ignored.
+//
+// ie. a handler may look like:
+//
+//  queue.HandlerFunc(func(r *bokchoy.Request) {
+// 	 ctx := r.Context()
+// 	 processTime := time.Duration(rand.Intn(4)+1) * time.Second
+//
+// 	 select {
+// 	 case <-ctx.Done():
+// 	 	return
+//
+// 	 case <-time.After(processTime):
+// 	 	 // The above channel simulates some hard work.
+// 	 }
+//
+// 	 return nil
+//  })
+//
+func Timeout(timeout time.Duration) func(next bokchoy.Handler) bokchoy.Handler {
+	return func(next bokchoy.Handler) bokchoy.Handler {
+		fn := func(r *bokchoy.Request) error {
+			ctx, cancel := context.WithTimeout(r.Context(), timeout)
+			var err error
+
+			defer func() {
+				cancel()
+				err = ctx.Err()
+			}()
+
+			r = r.WithContext(ctx)
+
+			next.Handle(r)
+
+			return err
+		}
+		return bokchoy.HandlerFunc(fn)
+	}
+}
