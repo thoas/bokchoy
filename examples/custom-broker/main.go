a11c9db9efc09b51514fda184ff0a2002816fe0a
Florent Messa <florent.messa@gmail.com>
2019-08-06 17:36:38 +0200

    docs: add custom broker example

---
+++ examples/custom-broker/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/go-redis/redis"
+
+	"github.com/thoas/bokchoy"
+	"github.com/thoas/bokchoy/logging"
+)
+
+func main() {
+	ctx := context.Background()
+
+	logger := logging.NewNopLogger()
+
+	clt := redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+	})
+
+	// define the main engine which will manage queues
+	engine, err := bokchoy.New(ctx, bokchoy.Config{}, bokchoy.WithBroker(&bokchoy.RedisBroker{
+		Client:     clt,
+		ClientType: "client",
+		Logger:     logger,
+	}))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	payload := map[string]string{
+		"data": "hello world",
+	}
+
+	task, err := engine.Queue("tasks.message").Publish(ctx, payload,
+		bokchoy.WithTimeout(1*time.Second), bokchoy.WithCountdown(-1))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(task, "has been published")
+}
